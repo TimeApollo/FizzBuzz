7cd5e859035aa6c3920e73bff29a4acce60af1b7
TimeApollo <arnjcksn0845@yahoo.com>
2018-04-25 15:22:44 -0400

    fixed some mistakes

--- script.js
+++ script.js
@@ -11,18 +11,18 @@ for(var i = 1; i<=1000; i++){
             }
         }
     }
-    if(i===2){
+    if(i===3){
         document.write('<span style="color:green;border-style:solid;border-color:red">',i,', </span>');//edge case
-    }else if(i===3){
+    }else if(i===2){
         document.write('<span style="border-style:solid;border-color:red"><strong>',i,'</strong>, </span>');//edge case
     }else if(i%3===0 && i%2===0 && i!==1000){
         document.write('<span style="color:green"><strong>',i,'</strong>, </span>');//make bold and green
-    }else if(i%3===0 && i%2!==0 && i!==1000){
-        document.write('<span><strong>',i,'</strong>, </span>');//make bold
     }else if(i%3!==0 && i%2===0 && i!==1000){
+        document.write('<span><strong>',i,'</strong>, </span>');//make bold
+    }else if(i%3===0 && i%2!==0 && i!==1000){
         document.write('<span style="color:green">',i,', </span>');//make green
     }else if(i===1000){
-        document.write('<span style="color:green">',i,'.</span>');//make green with period at end
+        document.write('<span><strong>',i,'.</strong></span>');//make green with period at end
     }else if(prime === 1){
         document.write('<span style="border-style:solid;border-color:red">',i,', </span>');//outline border
     }else{
